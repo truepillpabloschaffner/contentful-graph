9ddcef743ed04193dfa1f9090aa1c65ca5c68a22
truepillpabloschaffner <pabloschaffner@truepill.com>
2022-07-01 01:05:03 -0400

    added usage within entries times per type

--- src/content-types-to-model-map.js
+++ src/content-types-to-model-map.js
@@ -1,6 +1,16 @@
 const getRelations = require('./get-relations');
 
 /**
+ * Returns the amount of times a given type is used within the entries
+ * @param {string} type 
+ * @param {any} types 
+ * @returns {number}
+ */
+function numOccurrences(word,string) {
+  return (string.match(new RegExp(word, 'g')) || []).length;
+}
+
+/**
  * Convert Contentful model types definitions to a structure
  * that contains "relations" property, which can be used to build
  * dependency graph
@@ -11,18 +21,20 @@ const getRelations = require('./get-relations');
 function contentTypesToModelMap(typesx) {
   const modelsMap = {};
   const types = typesx.contentTypes;
+  const entries = JSON.stringify(typesx.entries);
   types.forEach((type) => {
     try {
       modelsMap[type.sys.id] = {
         name: type.name,
         fields: type.fields,
         relations: getRelations(type, types),
+        used: numOccurrences(type.sys.id,entries),
         //sys: type.sys,
       };
     } catch (e) {
     }
   });
-  //console.log('modelsMap', modelsMap);
+  console.log('modelsMap', modelsMap);
   return modelsMap;
 }
 
